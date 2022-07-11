4ecf70dc19816300790208c42c91ecdf7ea9ef4c
Georgyyx <julyfortune101@gmail.com>
2022-07-12 01:26:49 +0300

    released leveling system

--- tests/blinker_logic.js
+++ tests/blinker_logic.js
@@ -13,16 +13,14 @@ let data = JSON.parse(data_f)[0].flashes // data_f in nearby .json
 //I highly recommend to optimaze it 
 //considering monitor frequency
 //if you really know how rendering works ^^ 
-let frequency_per_minute = 100
-let show_tick_time = 60000 / frequency_per_minute
-let time_to_blink_msecs = 4000
+// let frequency_per_minute = 100
+
 
 let random_word = function(words_req_pool) {
     return words_req_pool[Math.floor(Math.random() * words_req_pool.length)]
 }
 
 let blinking_int = function(elements, words_req_pool){
-    // let jump_to = [...elements].filter(element => element.innerText == "")
     function is_texted(element, index, array){
         if (element.innerText != ""){
             return true
@@ -31,7 +29,7 @@ let blinking_int = function(elements, words_req_pool){
     }
     not_to_jump = [...elements].find(is_texted);
     let not_jump_to_ind = [...elements].indexOf(not_to_jump);
-    elements[not_jump_to_ind].innerText = ""
+    [...elements][not_jump_to_ind].innerText = ""
     el_ind = not_jump_to_ind
     while (el_ind == not_jump_to_ind) {
         el_ind = Math.floor(Math.random() * elements.length)
@@ -77,7 +75,7 @@ const create_blink_areas = () => {
 }
 
 const fill_answers = function() {
-    clearInterval(this.blink)
+    clearInterval(this)
     for (el of bl_el){
         el.style.display = 'none'
     }
@@ -114,27 +112,56 @@ const remove_answers = () => {
     ancontainer.remove()
 }
 
+const reset = () => {
+    localStorage.removeItem('user_freq')
+    clear_input()
+    get_level()
+}
+
 const check_last = function(event) {
     hide_answers()
     result = document.createElement('h4')
+    reset_btn = document.createElement('button')
+    continue_button = document.createElement('button')
+    buttons_legend_cont = document.createElement('div')
+    buttons_legend_cont.className = 'b-l-cont'
+    continue_button.innerHTML = 'Играем'
+    continue_button.onclick = stage
+    reset_btn.onclick = reset
+    reset_btn.innerText = 'Сброс настроек'
+    current_speed = document.createElement('p')
     if (globalThis.answer==event.target.textContent){
         remove_answers()
         result.innerText = 'Верно!'
-        this.frequency_per_minute += 50
+        globalThis.frequency_per_minute += 25
+        localStorage.setItem('user_freq', globalThis.frequency_per_minute)
     } else {
         remove_answers()
         result.innerText = "Неверно..."
-        this.frequency_per_minute -= 50
+        globalThis.frequency_per_minute -= 25
+        localStorage.setItem('user_freq', globalThis.frequency_per_minute)
     }
-    blink_area = document.getElementsByTagName('h2')[0]
-    document.body.insertBefore(result, blink_area)
+    freq_after_stage = globalThis.frequency_per_minute
+    current_speed.innerText = `Текущая частота - ${freq_after_stage}
+        обновлений в минуту`
+    document.body.appendChild(result)
+    document.body.appendChild(buttons_legend_cont)
+    buttons_legend_cont.appendChild(reset_btn)
+    buttons_legend_cont.appendChild(continue_button)
+    buttons_legend_cont.appendChild(current_speed)
 }
 
 
-
 let stage = function() {
+    clear_input()
     create_blink_areas()
 
+    freq_prep = globalThis.frequency_per_minute
+    if (freq_prep < 1) {
+        globalThis.frequency_per_minute = 30;
+        freq_prep = 25;
+    }
+    let show_tick_time = 60000 / freq_prep
     bl_el = document.getElementsByClassName('blink-area')
     let blink = setInterval(blinking_int, show_tick_time, bl_el,data)
     
@@ -150,4 +177,48 @@ let stage = function() {
     setTimeout(stage_decide, time_to_blink_msecs)
 }
 
-stage()
+
+const get_level = () => {
+    label = document.createElement('label')
+    label.innerText = "Введите начальную скорость:"
+    text_input = document.createElement('input')
+    text_input.type = 'text'
+    text_input.className = 'freq'
+    submit_btn = document.createElement('button')
+    submit_btn.className = 'input-button'
+    submit_btn.type = 'submit'
+    submit_btn.onclick = accept_input
+    submit_btn.innerText = 'Введено'
+    document.body.appendChild(label)
+    document.body.appendChild(text_input)
+    document.body.appendChild(submit_btn)
+}
+
+const clear_input = () => {
+    document.body.replaceChildren()
+}
+
+const accept_input = function() {
+    input_val = document.getElementsByClassName('freq')[0].value
+    if (isNaN(input_val) || Number(input_val) < 1) {
+        alert("Натуральным числом!")
+    } else {
+        window.localStorage.setItem('user_freq', input_val)
+        this.frequency_per_minute = Number(input_val)
+        // clear_input()
+        stage()
+    }
+}
+
+let time_to_blink_msecs = 4000
+
+if (localStorage.getItem('user_freq')===null){
+    get_level()
+} else {
+    freq = localStorage.getItem('user_freq')
+    this.frequency_per_minute = Number(freq)
+    stage()
+}
+
+
+// stage()
